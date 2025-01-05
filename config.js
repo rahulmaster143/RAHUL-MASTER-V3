0a95d7dd5270be0528ef4320c1546f5dadeedc87
RAHUL-MASTER <rahulhiran330@gmail.com>
2025-01-05 12:11:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "2c9AxRoC#2idz_QjbUhWWlkjM62Snu5hKE9pHu5vkMTlPOqED-4o",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348078582627",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "919356730236",
     AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "false",
-    MODE: process.env.MODE || "private",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/efeurhobo/Empire_X/main/lib/assets/empire.jpg",
+    MODE: process.env.MODE || "public",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://api.tioo.eu.org/file/24Mz9c3fxSHl.jpeg",
     PREFIX: process.env.PREFIX || ".",
     OWNER_REACT: process.env.OWNER_REACT || "false",
     AUTO_REACT: process.env.AUTO_REACT || "false",
-    OWNER_NAME: process.env.OWNER_NAME || "𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
-    BOT_NAME: process.env.BOT_NAME || "Empire_X",
-    CAPTION: process.env.CAPTION || "Made By 𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
+    OWNER_NAME: process.env.OWNER_NAME || "RAHUL-JI",
+    BOT_NAME: process.env.BOT_NAME || "RAHUL-MASTER-V3",
+    CAPTION: process.env.CAPTION || "Made By RAHUL-MASTER-V3",
 };
